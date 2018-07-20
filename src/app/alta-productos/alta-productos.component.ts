ade8b5d0c16aa2c8d848d5803dfb097cfdf78d9e
DESKTOP-OGQSA58\Esaul <esaul161@hotmail.com>
2018-07-20 17:03:07 -0500

    Validación de Id al ingresar un nuevo producto

--- src/app/alta-productos/alta-productos.component.ts
+++ src/app/alta-productos/alta-productos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as Datastore from 'nedb';
 import {FormControl, Validators, FormGroup, FormBuilder} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-alta-productos',
@@ -12,6 +13,7 @@ export class AltaProductosComponent implements OnInit {
   ProductoForm:  FormGroup;
    db = new Datastore({ filename: './Productos.db', autoload: true });
    producto: ProductoTemplate;
+   existeId = false;
   constructor(private fb: FormBuilder,  private router: Router, private route: ActivatedRoute) {
     this.ProductoForm = this.fb.group({
       Id: new FormControl(null, [Validators.required]),
@@ -21,7 +23,7 @@ export class AltaProductosComponent implements OnInit {
       PrecioVenta: new FormControl(null, [Validators.required]),
       Marca: new FormControl(null, []),
       Proveedor: new FormControl(null, []),
-      Fecha: new FormControl(null, []),
+      Fecha: new FormControl({value: moment()}, []),
     });
   }
 
@@ -30,6 +32,30 @@ export class AltaProductosComponent implements OnInit {
 
   save() {
     console.log(this.ProductoForm.value);
+    this.ProductoForm.get('Fecha').setValue(moment(this.ProductoForm.get('Fecha').value).format('DD/MM/YYYY'));
+    this.db.insert(this.ProductoForm.value, function (err, newDoc) {   // Callback is optional
+      // newDoc is the newly inserted document, including its _id
+      // newDoc has no key called notToBeSaved since its value was undefined
+      console.log(err);
+      console.log(newDoc);
+    });
+  }
+
+  verificaId() {
+    let flag = false;
+    this.db.find({ Id: this.ProductoForm.get('Id').value }, function (err, docs) {
+      // docs is [{ planet: 'Mars', system: 'solar' }]
+      console.log(err);
+      console.log(docs);
+      if (docs.length) {
+        flag = true;
+      } else {
+        flag = false;
+      }
+    });
+    setTimeout(() => {
+      this.existeId = flag;
+    }, 700);
   }
 }
 
